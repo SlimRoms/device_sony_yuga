3daf4633e83317f1dce83218fdc29cdc98276d46
olivieer <yves.olli@gmail.com>
2015-01-05 18:37:38 +0100

    yuga: Slim Bringup
    * Add cmhw back

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_yuga.mk \
-    $(LOCAL_DIR)/omni_yuga.mk
+    $(LOCAL_DIR)/full_yuga.mk \
+    $(LOCAL_DIR)/slim.mk
--- aosp_yuga.mk
+++ aosp_yuga.mk
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/sony/yuga/omni_yuga.mk)
+$(call inherit-product, device/sony/yuga/full_yuga.mk)
 
 PRODUCT_NAME := aosp_yuga
